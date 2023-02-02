9359ed4d9a17eac59106b881a06caa22a8082852
mpho.nyambeni-at-730528711512 <nyambenimphogiven@gmail.com>
2023-02-02 19:40:54 +0200

    admin

---
+++ admin/admin.controller.js
@@ -0,0 +1,81 @@
+﻿const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const authorize = require('_middleware/authorize')
+const userService = require('./user.service');
+
+// routes
+router.post('/ad/authenticate', authenticateSchema, authenticate);
+router.post('/ad/register', registerSchema, register);
+router.get('/ad/', authorize(), getAll);
+router.get('/ad/current', authorize(), getCurrent);
+router.get('/ad/:id', authorize(), getById);
+router.put('/ad/:id', authorize(), updateSchema, update);
+router.delete('/ad/:id', authorize(), _delete);
+
+module.exports = router;
+
+function authenticateSchema(req, res, next) {
+    const schema = Joi.object({
+        email: Joi.string().required(),
+        password: Joi.string().required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function registerSchema(req, res, next) {
+    const schema = Joi.object({
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+    });
+    validateRequest(req, next, schema);
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({ message: 'Registration successful' }))
+        .catch(next);
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function getCurrent(req, res, next) {
+    res.json(req.user);
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+        email: Joi.string().required(),
+        password: Joi.string().required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({ message: 'User deleted successfully' }))
+        .catch(next);
+}
